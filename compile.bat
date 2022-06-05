519d5ad5b81b819467266384734769c201452de7
Peter Zhang <ultraswordkirbypink@gmail.com>
2022-06-05 13:44:54 -0400

    added dependencies
    added dlls and also compile.bat

---
+++ compile.bat
@@ -0,0 +1,12 @@
+@echo off
+cls
+
+if exist "game.exe" (
+    del "game.exe"
+)
+
+g++ -I src -o "game.exe" "main.cpp" -lglfw3dll -luser32 -lopengl32 -lGdi32 -lShell32 -lglew32s -lglew32
+
+if not errorlevel 1 (
+    "game.exe"
+)
