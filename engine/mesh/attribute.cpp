9c3def1d683422fd01e7a5227c291ad14f753991
Peter Zhang <ultraswordkirbypink@gmail.com>
2022-06-25 13:05:40 -0400

    added offset for attributes

--- engine/mesh/attribute.cpp
+++ engine/mesh/attribute.cpp
@@ -1,8 +1,8 @@
 #include "attribute.hpp"
 
-Sora::Attribute Sora::create_attribute(int pointer, int size, int type, int normalized, int vertex_size)
+Sora::Attribute Sora::create_attribute(int pointer, int size, int type, int normalized, int vertex_size, int offset_bytes)
 {
-    return {pointer, size, type, normalized, vertex_size};
+    return {pointer, size, type, normalized, vertex_size, offset_bytes};
 }
 
 void Sora::Attribute::enable_attrib()
@@ -19,5 +19,7 @@ void Sora::Attribute::establish_attrib()
 {
     // std::cout << pointer << " " << size << " " << type << " " << normalized << " " << vertex_size_bytes << std::endl;
     this->enable_attrib();
-    glVertexAttribPointer(this->pointer, this->size, this->type, this->normalized, this->vertex_size_bytes, (void *)0);
+    glVertexAttribPointer(this->pointer, this->size, this->type, this->normalized, this->vertex_size_bytes,
+                          // if 64bit make 8 bytes -- else make 4 bytes | 8 bytes = long long, 4 bytes = int
+                          (_32BIT) ? (void *)(this->offset_bytes) : (void *)((long long)this->offset_bytes));
 }
--- engine/mesh/attribute.hpp
+++ engine/mesh/attribute.hpp
@@ -1,6 +1,8 @@
 #include <GL/glew.h>
 #include <iostream>
 
+#include "../engine.hpp"
+
 namespace Sora
 {
 
@@ -11,12 +13,13 @@ namespace Sora
         int type;
         int normalized;
         int vertex_size_bytes;
+        int offset_bytes;
 
         void enable_attrib();
         void disable_attrib();
         void establish_attrib();
     };
 
-    Attribute create_attribute(int pointer, int size, int type, int normalized, int vertex_size_bytes);
+    Attribute create_attribute(int pointer, int size, int type, int normalized, int vertex_size_bytes, int offset_bytes);
 
 }
