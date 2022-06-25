3c7dbea5ce03e51e43643f5671bd5fd9fffe90d0
Peter Zhang <ultraswordkirbypink@gmail.com>
2022-06-25 13:06:54 -0400

    testing Sora::Texture2D and glActiveTexture() methods

--- main.cpp
+++ main.cpp
@@ -37,6 +37,7 @@ int main()
     // testing shaders
     Sora::Shader shader = Sora::load_shader_from_file("assets/shaders/default.glsl");
     shader.create();
+    shader.bind();
 
     Sora::ShaderUtils::UniformMap u_map;
     u_map.set_entry(shader.uniform_location("utime"), "utime");
@@ -44,47 +45,59 @@ int main()
     // std::cout << u_map.get_entry("utime") << std::endl;
 
     // ---- create texture
-    Sora::Texture2D tex("assets/images/image.jpeg");
+    Sora::Texture2D tex("assets/images/tilemap.png");
     tex.create();
-    shader.uploadTexture2D("utex", &tex);
+
+    // bind texture once for now
+    glActiveTexture(GL_TEXTURE0);
+    tex.bind();
+    shader.uploadInt("utex", 0);
+    tex.unbind();
+
+    Sora::Texture2D tex2("assets/images/jirachi.jpg");
+    tex2.create();
+
+    glActiveTexture(GL_TEXTURE1);
+    tex2.bind();
+    shader.uploadInt("utex2", 1);
+    tex2.unbind();
 
     // --- end texture
 
     // vertices
     float vertices[] = {
-        // bottom left, bototm right, top right, top left
-        -1.0f, -1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
-        1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f,
-        1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f,
-        -1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f};
+        // positions          // colors           // texture coords
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,   // top right
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, // bottom left
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f   // top left
+    };
 
     // index stuff
     unsigned int indices[] = {
-        0, 1, 2,
-        2, 3, 0};
+        0, 1, 3,
+        1, 2, 3};
 
+    // create objects
     Sora::VAO m_vao;
     m_vao.create();
     m_vao.bind();
-
     Sora::BufferObject<float> vert(36, GL_ARRAY_BUFFER, GL_STATIC_DRAW);
     vert.create();
     vert.bind();
+    // data upload and set up
     vert.set_data(vertices, 36);
     vert.upload_data();
-
-    m_vao.add_attribute(Sora::create_attribute(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 9));
-    m_vao.add_attribute(Sora::create_attribute(1, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 9));
-    m_vao.add_attribute(Sora::create_attribute(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 9));
-
-    Sora::BufferObject<uint>
-        index(6, GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW);
+    int vertex_size_bytes = sizeof(float) * 9;
+    m_vao.add_attribute(Sora::create_attribute(0, 3, GL_FLOAT, GL_FALSE, vertex_size_bytes, 0));
+    m_vao.add_attribute(Sora::create_attribute(1, 4, GL_FLOAT, GL_FALSE, vertex_size_bytes, sizeof(float) * 3));
+    m_vao.add_attribute(Sora::create_attribute(2, 2, GL_FLOAT, GL_FALSE, vertex_size_bytes, sizeof(float) * 7));
+    Sora::BufferObject<uint> index(6, GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW);
     index.create();
     index.bind();
     index.set_data(indices, 6);
     index.upload_data();
-
-    // std::cout << vao << " " << vbo << " " << ibo << std::endl;
+    // unbind everything
     m_vao.disable_attribs();
     m_vao.unbind();
     vert.unbind();
@@ -105,14 +118,20 @@ int main()
         //     std::cout << "A";
 
         // render call
-        shader.bind();
+        glActiveTexture(GL_TEXTURE0);
         tex.bind();
+        glActiveTexture(GL_TEXTURE1);
+        tex2.bind();
+        shader.bind();
         shader.uploadValue(u_map.get_entry("utime"), Sora::Time::get_time());
         m_vao.bind();
         m_vao.enable_attribs();
-        glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, nullptr);
+        glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, 0);
+        m_vao.disable_attribs();
+        m_vao.unbind();
         shader.unbind();
         tex.unbind();
+        tex2.unbind();
 
         glCheckError();
 
@@ -129,6 +148,7 @@ int main()
     shader.unbind();
     shader.clean();
     tex.clean();
+    tex2.clean();
 
     window.clean();
     Sora::clean_engine(Sora::VERY_VERBOSE);
