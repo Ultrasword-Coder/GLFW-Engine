c1e1ca4dfc47c3309b810ab054734bac654cd399
Peter Zhang <ultraswordkirbypink@gmail.com>
2022-06-26 14:30:37 -0400

    added IRK/IKP dll files

--- compile.bat
+++ compile.bat
@@ -5,7 +5,7 @@ if exist "sora.exe" (
     del "sora.exe"
 )
 
-g++ -Isrc -Iinclude -o "sora.exe" "main.cpp" -lglfw3dll -luser32 -lopengl32 -lGdi32 -lShell32 -lglew32s -lglew32
+g++ -Isrc -Iinclude -o "sora.exe" "main.cpp" -lglfw3dll -luser32 -lopengl32 -lGdi32 -lShell32 -lglew32s -lglew32 -lirrKlang
 
 if not errorlevel 1 (
     "sora.exe"
