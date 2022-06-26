11353e88f1ba454a1849775ce47dd380c5c263d1
Peter Zhang <ultraswordkirbypink@gmail.com>
2022-06-25 22:19:21 -0400

    added transformation matrix to vertex shader

--- assets/shaders/default.glsl
+++ assets/shaders/default.glsl
@@ -4,11 +4,13 @@ layout(location=0) in vec3 vPos;
 layout(location=1) in vec4 vCol;
 layout(location=2) in vec2 vTex;
 
+uniform mat4 transform;
+
 out vec4 fCol;
 out vec2 fTex;
 
 void main(){
-    gl_Position = vec4(vPos, 1.0);
+    gl_Position = transform * vec4(vPos, 1.0);
     fCol = vCol;
     fTex = vTex;
 }
